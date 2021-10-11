d4fcb8d4281375f9db8beb1de07fd787348035bb
Maria Sandoval <marysandovalrodriguez@hotmail.com>
2021-10-11 12:30:04 -0500

    Migración a react de la interfaz Gestión de ingreso

---
+++ Interfaces/GestionIngreso/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <p>Cuida tu seguridad digital, te invitamos a cambiar tu contraseña y activar el doble factor de autenticación
+                de correo electrónico.
+             </p>
+        </footer>
+    )
+}
+
+export default Footer
