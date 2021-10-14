54154fe28a0f0763ee00739dc99f64cedd2b697d
AdrianArg32 <jessyargoty32@gmail.com>
2021-10-14 10:58:59 -0500

    Actualizacion a react

---
+++ Interfaces/mventas/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function header(props) {
+    return (
+        <div>
+            <header>
+      <h1 id="titulo">Maestro de Ventas</h1>
+    </header>
+        </div>
+    )
+}
+
+export default header
