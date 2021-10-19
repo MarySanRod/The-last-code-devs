0068b1ad6489da1ef6d4617286e0befa422a8ab9
Maria Sandoval <marysandovalrodriguez@hotmail.com>
2021-10-19 16:47:07 -0500

    Se actualiza presentación de la interfaz Gestión de Ingreso, se inserta nuevo código

--- 1- FRONTEND/GestionIngreso/src/App.js
+++ 1- FRONTEND/GestionIngreso/src/App.js
@@ -2,16 +2,23 @@ import React from "react";
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Form from "./components/Form";
+// import Form from "./components/Form";
 import Login from "./components/Login";
+import Logo from "./components/Logo";
 
 function App() {
     return(
-        <div>
+        <div className = 'Container'>
             <Header/>
-            <Form/>
+            
+            <div className = 'Ingreso'>
+                <Logo/>
+                <Login/>
+            </div>
+            
+            {/* <Form/> */}
             <Footer/>
-            <Login/>
+            
         </div>
     );
 }
