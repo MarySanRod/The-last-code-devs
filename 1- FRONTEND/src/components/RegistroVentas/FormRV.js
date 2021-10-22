bde4a3cb42e4455c049eb50e7ab129dbda0cb80d
Maria Sandoval <marysandovalrodriguez@hotmail.com>
2021-10-22 13:10:27 -0500

    Se actualizan estilos interfaz Registro Ventas

--- 1- FRONTEND/src/components/RegistroVentas/FormRV.js
+++ 1- FRONTEND/src/components/RegistroVentas/FormRV.js
@@ -29,35 +29,35 @@ function FormRV() {
         <form id= 'formRV' onSubmit = {registrarVenta}>
             <div id="cliente">
                 <label id= 'RV'> ID cliente</label>
-                <input id= 'RV' type="text" required placeholder="ID cliente" onChange={e=>setIdCliente(e.target.value)}/>
+                <input id= 'RV1' type="text" required placeholder="ID cliente" onChange={e=>setIdCliente(e.target.value)}/>
                 <label id= 'RV' > Nombre del cliente</label>
-                <input id= 'RV' type= "text"  require placeholder="Nombre del cliente" onChange={e=>setNomCliente(e.target.value)}/>
+                <input id= 'RV1' type= "text"  require placeholder="Nombre del cliente" onChange={e=>setNomCliente(e.target.value)}/>
             </div>
             <div id ="vendedor">
                 <label id= 'RV'> ID Vendedor</label>
-                <input id= 'RV' type="text" required placeholder="ID vendedor" onChange={e=>setIdVendedor(e.target.value)}/>
+                <input id= 'RV1' type="text" required placeholder="ID vendedor" onChange={e=>setIdVendedor(e.target.value)}/>
                 <label id= 'RV'> Nombre del vendedor</label>
-                <input id= 'RV' type= "text" required placeholder="Nombre del vendedor" onChange={e=>setNomVendedor(e.target.value)}/>
+                <input id= 'RV1' type= "text" required placeholder="Nombre del vendedor" onChange={e=>setNomVendedor(e.target.value)}/>
             </div>
             <div id= "fechas">
                 <label id= 'RV'> Fecha inicial</label>
-                <input id= 'RV' type="date" required placeholder="fecha inicial" onChange={e=>setFechaCompra(e.target.value)}/>
+                <input id= 'RV1' type="date" required placeholder="fecha inicial" onChange={e=>setFechaCompra(e.target.value)}/>
                 <label id= 'RV'> Fecha de pago</label>
-                <input id= 'RV' type= "date" required placeholder="fecha de pago" onChange={e=>setFechaPago(e.target.value)}/>   
+                <input id= 'RV1' type= "date" required placeholder="fecha de pago" onChange={e=>setFechaPago(e.target.value)}/>   
             </div>
             <div id="productos">
                 <label id= 'RV'> ID Producto</label>
-                <input id= 'RV' type="text" required placeholder="ID producto" onChange={e=>setIdProducto(e.target.value)}/>
+                <input id= 'RV1' type="text" required placeholder="ID producto" onChange={e=>setIdProducto(e.target.value)}/>
                 <label id= 'RV'> Precio del producto</label>
-                <input id= 'RV' type= "number"  required placeholder="precio del producto" onChange={e=>setPreProducto(e.target.value)}/>
+                <input id= 'RV1' type= "number"  required placeholder="precio del producto" onChange={e=>setPreProducto(e.target.value)}/>
                 <label id= 'RV'> Cantidad</label>
-                <input id= 'RV' type= "number" required placeholder="cantidad del producto" onChange={e=>setCantidad(e.target.value)}/>
+                <input id= 'RV1' type= "number" required placeholder="cantidad del producto" onChange={e=>setCantidad(e.target.value)}/>
             </div>
             <div id="venta">
                 <label id= 'RV'> ID venta</label>
-                <input id= 'RV' type="text" required placeholder="ID venta" onChange={e=>setIdVenta(e.target.value)}/><br></br><br></br>
+                <input id= 'RV1' type="text" required placeholder="ID venta" onChange={e=>setIdVenta(e.target.value)}/><br></br><br></br>
                 <label id= 'RV'> Descripción de la venta</label><br></br>
-                <textarea cols="50" rows="5" placeholder="ingrese descripción de la venta" onChange={e=>setDescripcion(e.target.value)}>
+                <textarea id= 'desc' cols="50" rows="5" placeholder="ingrese descripción de la venta" onChange={e=>setDescripcion(e.target.value)}>
                 </textarea>
             </div>
             <div>
