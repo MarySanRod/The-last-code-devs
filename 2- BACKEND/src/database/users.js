eb3f10a60b1b71a13c18a901df310b06a655f0d5
MarySanRod <90804645+MarySanRod@users.noreply.github.com>
2021-10-24 13:02:24 -0500

    Se actualiza modelo y esquema Users

--- 2- BACKEND/src/database/users.js
+++ 2- BACKEND/src/database/users.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const usersSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  admin: { type: Boolean, required: true, default: false }
+  admin: { type: Boolean, required: true, default: false },
+  rol: { type: String, required: true, default: 'Pendiente' }
 });
 const usersModel = mongoose.model('users', usersSchema);
 module.exports = usersModel;
